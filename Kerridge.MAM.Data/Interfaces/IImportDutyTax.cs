5eb8d88467e4c0a7e9596a5f716d41064d770a8f
Faith-ZAR <sisazaza1@gmail.com>
2024-06-20 05:56:28 +0200

    Add project files.

---
+++ Kerridge.MAM.Data/Interfaces/IImportDutyTax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kerridge.MAM.Data.Interfaces
+{
+    internal interface IImportDutyTax
+    {
+        double CalculateImportDutyTax(double taxPercentage, double price);
+    }
+}
---
+++ Kerridge.MAM.Data/Models/Tax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kerridge.MAM.Data.Models
+{
+    public class Tax
+    {
+        public int TaxId { get; set; }
+        public string Description { get; set; }
+        public double Percentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; }
+    }
+}
